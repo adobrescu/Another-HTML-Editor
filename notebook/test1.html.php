75ebc0d6e21d1cba5f3d5018790e3a33b3a8e27c
Adi Dobrescu <adobrescu@insolitservices.ro>
2016-04-12 15:42:31 +0300

    Modifies EditableContent::collectInsertionBoundaries to skip nodes that are not editable
    - contentEditable=false or if contentEditable=="inherit" one of the ascendants has contentEditable=="false"

--- notebook/test1.html.php
+++ notebook/test1.html.php
@@ -64,12 +64,14 @@
 			ed.addEditableContent(document.getElementById("ifrm1"));
 			ed.addEditableContent(document.getElementById("ifrm2"));
 		}
+		function showHTML()
+			{
+				document.getElementById('htmlView').value=ed.currentEditableContent.documentContainer.innerHTML;
+			}
 		function surround(tagName, tagAttributes)
 			{
-				if(!ed.currentEditableContent)
-				{
-					return;
-				}
+				
+				console.clear();
 				var strTagAttributes
 				if(!tagAttributes)
 				{
@@ -89,12 +91,13 @@
 					var attr=arrTagAttributes[i].split("=");
 					tagAttributes[attr[0]]=attr[1];
 				}
-
-				ed.currentEditableContent.surroundContentFragment(tagName?tagName:document.getElementById("tagName").value, 
+				
+				ed.currentEditableContent.surroundSelectedContentFragments(tagName, 
 									tagAttributes,
 									true);
 				
-				
+				showHTML();
+				ed.currentEditableContent.documentContainer.focus();
 			}
 		</script>
 	</head>
@@ -108,80 +111,15 @@
 			
 			<iframe src="template2.html" id="ifrm2"></iframe>
 		</div>
-		<div id="toolbox">
-			<a href="#" onclick="if(ed.currentEditableContent){ed.currentEditableContent.undo()}; return false;">Undo</a>
+		<div>
+			<a href="#" onclick="updateHTML(); return false;">Update HTML</a>
 			<br>
-			<a href="#" onclick="if(ed.currentEditableContent){ed.currentEditableContent.redo()}; return false;">Redo</a>
-			<br>
-			<a href="#" onclick="surround(document.getElementById('selectTagName').value, document.getElementById('selectTagAttributes').value) ; return false;">Surround with </a>
-			<select id="selectTagName" onchange="if(!ed.currentEditableContent.getRange().collapsed){surround(document.getElementById('selectTagName').value, document.getElementById('selectTagAttributes').value) ;}">
-				<optgroup label="Sectioning">
-					<option value="ADDRESS">ADDRESS</option>
-					<option value="ARTICLE">ARTICLE</option>
-					<option value="ASIDE">ASIDE</option>
-					<option value="FOOTER">FOOTER</option>
-					<option value="H1">H1</option>
-					<option value="H2">H2</option>
-					<option value="H3">H3</option>
-					<option value="H4">H4</option>
-					<option value="H5">H5</option>
-					<option value="H6">H6</option>
-					<option value="HEADER">HEADER</option>
-					<option value="NAV">NAV</option>
-					<option value="SECTION">SECTION</option>
-				</optgroup>
-				<optgroup label="Grouping">
-					<option value="BLOCKQUOTE">BLOCKQUOTE</option>
-					<option value="P">P</option>
-					<option value="HR">HR</option>
-					<option value="BR">BR</option>
-					<option value="PRE">PRE</option>
-					<option value="DIALOG">DIALOG</option>
-					<option value="OL">OL</option>
-					<option value="UL">UL</option>
-					<option value="LI">LI</option>
-					<option value="DL">DL</option>
-					<option value="DT">DT</option>
-					<option value="DD">DD</option>
-				</optgroup>
-				<optgroup label="Text-Level Semantics">
-					<option value="A">A</option>
-					<option value="ABBR">ABBR</option>
-					<option value="B" selected>B</option>
-					<option value="BDO">BDO</option>
-					<option value="CITE">CITE</option>
-					<option value="CODE">CODE</option>
-					<option value="DFN">DFN</option>
-					<option value="EM">EM</option>
-					<option value="I">I</option>
-					<option value="KBD">KBD</option>
-					<option value="MARK">MARK</option>
-					<option value="METER">METER</option>
-					<option value="PROGRESS">PROGRESS</option>
-					<option value="RP">RP</option>
-					<option value="RT">RT</option>
-					<option value="RUBY">RUBY</option>
-					<option value="Q">Q</option>
-					<option value="SAMP">SAMP</option>
-					<option value="SMALL">SMALL</option>
-					<option value="SPAN">SPAN</option>
-					<option value="STRONG">STRONG</option>
-					<option value="SUB">SUB</option>
-					<option value="SUP">SUP</option>
-					<option value="TIME">TIME</option>
-					<option value="VAR">VAR</option>
-				</optgroup>
-				<optgroup label="Edits">
-					<option value="DEL">DEL</option>
-					<option value="INS">INS</option>
-				</optgroup>
-				<optgroup label="None">
-					<option value="DIV">DIV</option>
-				</optgroup>
-			</select>
-			, attributes <input type="text" id="selectTagAttributes" size="26" value="">
+			<textarea style="border: 1px #c9c9c9  solid;" id="htmlView"></textarea>
 		</div>
-		<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-		<div contenteditable="true" style="border: 1px red solid; height: 100px;"></div>
+		<div id="toolbox">
+			<?php
+				include('quick-toolbar.html.php');
+			?>
+		</div>
 	</body>
 </html> 
